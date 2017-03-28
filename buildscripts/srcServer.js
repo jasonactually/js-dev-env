891f5f16fb6068f966478d54688070db1c5f65ee
JASON DAVIS <JASON DAVIS>
2017-03-27 21:12:10 -0600

    Missed a file

--- buildscripts/srcServer.js
+++ buildscripts/srcServer.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 
-var port = 3000;
+const port = 3000;
 var app = express();
 
 app.get('/', function(req, res){
